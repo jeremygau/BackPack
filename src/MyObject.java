580f3a6bb4c146276d11568d367b2149660f8e99
jeremy.gau <caliban3010@gmail.com>
2020-03-19 12:28:15 +0100

    Classe MyObject

---
+++ src/MyObject.java
@@ -0,0 +1,10 @@
+public class MyObject {
+
+    private int weight;
+    private int value;
+
+    public MyObject(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+}
