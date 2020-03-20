333be365331a3e31f6b132b0d744035b7db7a82a
jeremy.gau <caliban3010@gmail.com>
2020-03-20 20:02:53 +0100

    Comparaison avec une Pile

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,37 @@ import java.util.spi.AbstractResourceBundleProvider;
 
 public class Main {
     public static void main(String[] args) {
-        double moyenne = 0;
-        int repetition  = 100;
-        for (int i = 0; i < repetition; i++) {
+//        double moyenne = 0;
+//        int repetition  = 1;
+//        for (int i = 0; i < repetition; i++) {
+//
+//        }
+//        moyenne = moyenne / repetition;
+//        System.out.println(moyenne + " ms");
+        String filename = "./files/sac1_2.txt";
+        solveWithArray(filename);
+        solveWithStack(filename);
+    }
 
-            long before = System.currentTimeMillis();
-
-            Solver solver = new Solver("./files/sac1_2.txt");
+    public static void solveWithArray(String filename) {
+        long before = System.currentTimeMillis();
+        Solver solver = new Solver(filename);
 //            System.out.println(solver.getObjectArrayList());
-            System.out.println(solver.solve());
-//            System.out.println("bag capacity = " + solver.getBagCapacity());
-//            System.out.println("better solution weight = " + solver.getTotalWeight(solver.getBetterSolution()));
-//            System.out.println("better solution value = " + solver.getTotalValue(solver.getBetterSolution()));
+        System.out.println(solver.solve());
+        System.out.println("bag capacity = " + solver.getBagCapacity());
+        System.out.println("better solution weight = " + solver.getTotalWeight(solver.getBetterSolution()));
+        System.out.println("better solution value = " + solver.getTotalValue(solver.getBetterSolution()));
+        System.out.println("time = " + (System.currentTimeMillis() - before) + " ms");
+    }
 
-//            System.out.println("time = " + (System.currentTimeMillis() - before) + " ms");
-            moyenne += System.currentTimeMillis() - before;
-        }
-        moyenne = moyenne / repetition;
-        System.out.println(moyenne + " ms");
+    public static void solveWithStack(String filename) {
+        long before = System.currentTimeMillis();
+        SolverStack solverStack = new SolverStack(filename);
+//            System.out.println(solverStack.getObjectArrayList());
+        System.out.println(solverStack.solve());
+        System.out.println("bag capacity = " + solverStack.getBagCapacity());
+        System.out.println("better solution weight = " + solverStack.getTotalWeight(solverStack.getBetterSolution()));
+        System.out.println("better solution value = " + solverStack.getTotalValue(solverStack.getBetterSolution()));
+        System.out.println("time = " + (System.currentTimeMillis() - before) + " ms");
     }
 }
--- src/SolverStack.java
+++ src/SolverStack.java
@@ -1,27 +1,28 @@
+import java.util.Stack;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Solver {
+public class SolverStack {
 
     private int bagCapacity;
     private ArrayList<MyObject> objectArrayList;
     private ArrayList<MyObject> betterSolution;
 
-    public Solver(String filename) {
+    public SolverStack(String filename) {
         this.betterSolution = new ArrayList<>();
         this.objectArrayList = new ArrayList<>();
         readFile(filename);
     }
 
     public ArrayList<MyObject> solve() {
-        backtrack(new ArrayList<MyObject>(), 0, objectArrayList);
+        backtrack(new Stack<MyObject>(), objectArrayList);
         return betterSolution;
     }
 
-    public void backtrack(ArrayList<MyObject> a, int k, ArrayList<MyObject> input) {
+    public void backtrack(Stack<MyObject> a, ArrayList<MyObject> input) {
 //        System.out.println(a);
         if (isSolution(a)) {
             processSolution(a);
@@ -29,33 +30,41 @@ public class Solver {
         ArrayList<MyObject> candidates = constructCandidates(a, input);
         for (MyObject candidate : candidates) {
             if (getTotalWeight(a) + candidate.getWeight() <= bagCapacity) {
-                a.add(k, candidate);
-                backtrack(a, k + 1, input);
-                a.remove(k);
+                a.push(candidate);
+                backtrack(a, input);
+                a.pop();
             }
         }
     }
 
-    private boolean isSolution(ArrayList<MyObject> a) {
+    private boolean isSolution(Stack<MyObject> a) {
         return getTotalWeight(a) <= bagCapacity;
     }
 
-    private void processSolution(ArrayList<MyObject> a) {
+    private void processSolution(Stack<MyObject> a) {
         if (getTotalValue(a) > getTotalValue(this.betterSolution)) {
             betterSolution.clear();
             betterSolution.addAll(a);
         }
     }
 
-    public ArrayList<MyObject> constructCandidates(ArrayList<MyObject> a, ArrayList<MyObject> input) {
+    public ArrayList<MyObject> constructCandidates(Stack<MyObject> a, ArrayList<MyObject> input) {
         ArrayList<MyObject> list = new ArrayList<>();
         int start = 0;
-        if (!a.isEmpty())
-            start = input.indexOf(a.get(a.size() - 1)) + 1;
+        if (!a.empty())
+            start = input.indexOf(a.peek())+1;
         for (int i = start; i < input.size(); i++) {
             list.add(input.get(i));
         }
         return list;
+    }
+
+    public int getTotalWeight(Stack<MyObject> list) {
+        int weight = 0;
+        for (MyObject o : list) {
+            weight += o.getWeight();
+        }
+        return weight;
     }
 
     public int getTotalWeight(ArrayList<MyObject> list) {
@@ -64,6 +73,14 @@ public class Solver {
             weight += o.getWeight();
         }
         return weight;
+    }
+
+    public int getTotalValue(Stack<MyObject> list) {
+        int value = 0;
+        for (MyObject o : list) {
+            value += o.getValue();
+        }
+        return value;
     }
 
     public int getTotalValue(ArrayList<MyObject> list) {
